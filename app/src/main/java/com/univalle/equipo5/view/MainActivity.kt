044ac86f61b2bee20d252f50b25532e7a44da13e
Giordy <henaogiordy@gmail.com>
2024-10-13 14:19:54 -0500

    first commit

---
+++ app/src/main/java/com/univalle/equipo5/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.univalle.equipo5.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.univalle.equipo5.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
