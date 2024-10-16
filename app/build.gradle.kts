0f1aff130cd5fe8f60a3eadf61069158c6f36d76
Giordy <henaogiordy@gmail.com>
2024-10-16 13:15:02 -0500

    Api para el splash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //API para el Splash
+    implementation(libs.androidx.core.splashscreen)
 }
