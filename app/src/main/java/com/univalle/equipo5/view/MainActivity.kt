1b79bf436a6551c4b7039c5d93be5d1cce3ef538
Giordy <henaogiordy@gmail.com>
2024-10-16 17:48:26 -0500

    5 segundos

--- app/src/main/java/com/univalle/equipo5/view/MainActivity.kt
+++ app/src/main/java/com/univalle/equipo5/view/MainActivity.kt
@@ -7,7 +7,7 @@ import com.univalle.equipo5.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Thread.sleep(2000)
+        Thread.sleep(5000)
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
