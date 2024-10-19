fb92a373a8b2f1f1812b2efbf4e74f1eee78209a
karol arias <karol.ospina@correounivalle.edu.co>
2024-10-19 11:32:22 -0500

    add binding in toolbar

--- app/src/main/java/com/univalle/equipo5/view/HomeMain.kt
+++ app/src/main/java/com/univalle/equipo5/view/HomeMain.kt
@@ -8,8 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.findNavController
 import com.univalle.equipo5.R
+import com.univalle.equipo5.databinding.FragmentHomeMainBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +19,8 @@ import com.univalle.equipo5.R
 class HomeMain : Fragment() {
     private var mediaPlayer: MediaPlayer? = null
     private var isSoundOn: Boolean = true
+    private var _binding: FragmentHomeMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,61 +33,65 @@ class HomeMain : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_home_main, container, false)
+        // Inflar el layout usando DataBinding
+        _binding = FragmentHomeMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Configurar el Toolbar desde el fragment usando DataBinding
+        val toolbar = binding.customToolbar
+        (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
+
+        // Asignar animación y funcionalidad a cada ícono usando binding
         val scaleAnimation = android.view.animation.AnimationUtils.loadAnimation(context, R.anim.scale_animation)
 
-        val rateIcon: ImageView = rootView.findViewById(R.id.rate)
-        val soundIcon: ImageView = rootView.findViewById(R.id.sound)
-        val instructionsIcon: ImageView = rootView.findViewById(R.id.instructions)
-        val addIcon: ImageView = rootView.findViewById(R.id.add)
-        val shareIcon: ImageView = rootView.findViewById(R.id.share)
-
-        // Asignar animación y funcionalidad a cada ícono
-        rateIcon.setOnClickListener {
+        binding.rate.setOnClickListener {
             it.startAnimation(scaleAnimation)
             // Navegar a la siguiente pantalla o realizar otra acción
         }
 
-        soundIcon.setImageResource(R.drawable.sound)
+        binding.sound.setImageResource(R.drawable.sound)
 
-        soundIcon.setOnClickListener {
+        binding.sound.setOnClickListener {
             it.startAnimation(scaleAnimation)
             if (isSoundOn) {
-                // Si el sonido está encendido, se apaga y cambia el ícono
                 mediaPlayer?.pause()
-                soundIcon.setImageResource(R.drawable.nosound) // Cambia al ícono de sonido apagado
+                binding.sound.setImageResource(R.drawable.nosound) // Cambia al ícono de sonido apagado
             } else {
-                // Si el sonido está apagado, se enciende y cambia el ícono
                 mediaPlayer?.start()
-                soundIcon.setImageResource(R.drawable.sound) // Cambia al ícono de sonido encendido
+                binding.sound.setImageResource(R.drawable.sound) // Cambia al ícono de sonido encendido
             }
-            // Cambia el estado del sonido
             isSoundOn = !isSoundOn
         }
 
-        instructionsIcon.setOnClickListener {
+        binding.instructions.setOnClickListener {
             it.startAnimation(scaleAnimation)
             // Navegar a la fragment Instructions
             // findNavController().navigate(R.id.fragmentInstructions)
         }
 
-        addIcon.setOnClickListener {
+        binding.add.setOnClickListener {
             it.startAnimation(scaleAnimation)
             // Navegar a la siguiente pantalla o realizar otra acción
         }
 
-        shareIcon.setOnClickListener {
+        binding.share.setOnClickListener {
             it.startAnimation(scaleAnimation)
             // Navegar a la siguiente pantalla o realizar otra acción
         }
-
-        return rootView
     }
 
     override fun onPause() {
         super.onPause()
         mediaPlayer?.pause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     override fun onDestroy() {
@@ -94,22 +100,10 @@ class HomeMain : Fragment() {
         mediaPlayer = null
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Vincular el Toolbar desde el fragment
-        val toolbar: androidx.appcompat.widget.Toolbar = view.findViewById(R.id.custom_toolbar)
-
-        // Configura el toolbar como la ActionBar de la actividad
-        (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
-
-    }
-
     companion object {
         @JvmStatic
-        fun newInstance() =
-            HomeMain().apply {
-
-            }
+        fun newInstance() = HomeMain().apply {}
     }
 }
+
+
