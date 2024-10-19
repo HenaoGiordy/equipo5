bf11541dced4075f802d6aac5191010f2570af68
karol arias <karol.ospina@correounivalle.edu.co>
2024-10-19 00:53:09 -0500

    sound

--- app/src/main/java/com/univalle/equipo5/view/HomeMain.kt
+++ app/src/main/java/com/univalle/equipo5/view/HomeMain.kt
@@ -1,5 +1,6 @@
 package com.univalle.equipo5.view
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -21,16 +22,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeMain : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        mediaPlayer = MediaPlayer.create(requireContext(), R.raw.soundpokemon)
+        mediaPlayer?.isLooping = true
     }
 
     override fun onCreateView(
@@ -75,6 +72,22 @@ class HomeMain : Fragment() {
         }
 
         return rootView
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mediaPlayer?.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer?.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
