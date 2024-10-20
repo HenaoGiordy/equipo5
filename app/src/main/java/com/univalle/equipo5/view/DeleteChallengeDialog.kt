62d5fc5ac525cc6df2bcb976a828d451f211a513
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-10-20 18:12:08 -0400

    Cambios para no usar viewBinding en el dialogo de eliminar.

--- app/src/main/java/com/univalle/equipo5/view/DeleteChallengeDialog.kt
+++ app/src/main/java/com/univalle/equipo5/view/DeleteChallengeDialog.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import com.univalle.equipo5.R
 import com.univalle.equipo5.databinding.DialogDeleteChallengeBinding
 
 class DeleteChallengeDialog(
@@ -19,16 +21,16 @@ class DeleteChallengeDialog(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Usar ViewBinding para inflar el layout
-        _binding = DialogDeleteChallengeBinding.inflate(inflater, container, false)
+        // Usamos DataBindingUtil para inflar el layout del diálogo
+        _binding = DataBindingUtil.inflate(inflater, R.layout.dialog_delete_challenge, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Configurar la descripción del reto
-        binding.tvChallengeDescription.text = challengeDescription
+        // Configurar la descripción del reto usando DataBinding
+        binding.challengeDescription = challengeDescription
 
         // Acción al hacer clic en el botón "NO"
         binding.tvNo.setOnClickListener {
