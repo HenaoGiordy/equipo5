1d7cc0dcf80d3d09bb24d261edef8405551cb3da
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-10-22 15:09:18 -0500

    Uso de databinding para el fragment de instrucciones.

--- app/src/main/java/com/univalle/equipo5/view/Instructions.kt
+++ app/src/main/java/com/univalle/equipo5/view/Instructions.kt
@@ -1,74 +1,39 @@
 package com.univalle.equipo5.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.univalle.equipo5.R
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.databinding.DataBindingUtil
+import com.univalle.equipo5.R
+import com.univalle.equipo5.databinding.FragmentInstructionsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Instructions.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Instructions : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentInstructionsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        // Usamos DataBindingUtil para inflar el layout
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Busca el botón en el layout por su ID
-        val backButton = view.findViewById<View>(R.id.backButton)
-
-        // Configura el clic del botón para navegar a HomeMain
-        backButton.setOnClickListener {
-            // Navega a HomeMain usando el NavController
+        // Configurar el clic del botón para navegar a HomeMain
+        binding.backButton.setOnClickListener {
             findNavController().navigate(R.id.action_instructions_to_homeMain)
         }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Instructions.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Instructions().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = Instructions()
     }
 }
