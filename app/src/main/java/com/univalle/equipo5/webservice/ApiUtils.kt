6bd567be83d09aa5675d1e1be7ff3bac0b7f7530
Giordy <henaogiordy@gmail.com>
2024-11-30 16:46:11 -0500

    Configuracion de api

--- app/src/main/java/com/univalle/equipo5/webservice/ApiUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.univalle.equipo5.webservice
-
-class ApiUtils {
-    companion object{
-        fun getApiService():ApiService{
-            return RetrofitClient.getRetrofit().create(ApiService::class.java)
-        }
-    }
-}
